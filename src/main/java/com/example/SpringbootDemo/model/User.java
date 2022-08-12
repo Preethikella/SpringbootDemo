f92dfb6b2c5cc86fad46bfb9c2af18cfef2e2dbe
Preethi Kella <pavanipreethi.kella@imaginnovate.com>
2022-08-12 14:28:19 +0530

    modified

--- src/main/java/com/example/SpringbootDemo/model/User.java
+++ src/main/java/com/example/SpringbootDemo/model/User.java
@@ -1,17 +1,58 @@
 package com.example.SpringbootDemo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import lombok.Data;
+import javax.persistence.Table;
 
 @Entity
-@Data
-public class User {
+@Table(name= "user")
+public class User { 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	private int userId; 
+	private long userId; 
 	private String title;
 	private String body;
+	
+	public User(){
+		
+	}
+	
 
+	public User(long userId, String title, String body) {
+		super();
+		this.userId = userId;
+		this.title = title;
+		this.body = body;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	
 }
